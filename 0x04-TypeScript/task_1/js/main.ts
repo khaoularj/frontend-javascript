5039af0e47cd5ff77e2a8d020e19012b49e8fc3e
khaoularj <khaoula.raja1@gmail.com>
2024-12-12 15:02:54 +0000

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -32,4 +32,16 @@ const director1: Directors = {
 };
 
 
-console.log(director1);
+//console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+};
+
+
+console.log(printTeacher("John", "Doe")); 
